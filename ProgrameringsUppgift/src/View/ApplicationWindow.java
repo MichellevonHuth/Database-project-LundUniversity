6c0f8839c064b9c278bc845c3065a68fce033e96
Michele von Huth <m.j.s.vonhuth@se18.qmul.ac.uk>
2020-09-17 17:54:03 +0200

    teeeest

--- ProgrameringsUppgift/src/View/ApplicationWindow.java
+++ ProgrameringsUppgift/src/View/ApplicationWindow.java
@@ -10,7 +10,7 @@ public class ApplicationWindow {
 		Course course = new Course();
 		Student student = new Student();
 		
-		System.out.println("Hej");
+
 		
 		try {
 			DataBaseConnection DAL = new DataBaseConnection();
