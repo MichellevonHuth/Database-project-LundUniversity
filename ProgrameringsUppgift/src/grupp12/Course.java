de037f30115719112288c80bc21293028d3793a4
Idacarlsson <56579039+Idacarlsson@users.noreply.github.com>
2020-09-17 13:00:53 +0200

    Update Course.java

--- ProgrameringsUppgift/src/grupp12/Course.java
+++ ProgrameringsUppgift/src/grupp12/Course.java
@@ -2,4 +2,33 @@ package grupp12;
 
 public class Course {
 
+	private String courseCode;
+	private int credits;
+	private String name;
+	
+	public String getCourseCode() {
+		return courseCode;
+	}
+	
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+	public int getCredits() {
+		return credits;
+	}
+	
+	public void setCredits(int credits) {
+		this.credits = credits;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+	
+
 }
