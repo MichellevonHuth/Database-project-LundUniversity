66cf5f8cc5ea5e1fb23d4ad9678fc557d688ab0a
Idacarlsson <56579039+Idacarlsson@users.noreply.github.com>
2020-09-17 13:02:11 +0200

    Update GradeResult.java

--- ProgrameringsUppgift/src/grupp12/GradeResult.java
+++ ProgrameringsUppgift/src/grupp12/GradeResult.java
@@ -2,4 +2,14 @@ package grupp12;
 
 public class GradeResult {
 
+	private String letterGrade;
+
+	public String getLetterGrade() {
+		return letterGrade;
+	}
+
+	public void setLetterGrade(String letterGrade) {
+		this.letterGrade = letterGrade;
+	}
+	
 }
