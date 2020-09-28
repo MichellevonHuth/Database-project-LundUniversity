a74b6e97cadb2fba728d58bd2f683e53e92bb97a
Michele von Huth <m.j.s.vonhuth@se18.qmul.ac.uk>
2020-09-28 10:02:16 +0200

    uppgift3

--- ProgrammingAssignment3/src/controller/Controller.java
+++ ProgrammingAssignment3/src/controller/Controller.java
@@ -1,10 +1,19 @@
 package controller;
 
+
 import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.Vector;
+import javax.swing.JFileChooser;
+import java.lang.AutoCloseable;
+import java.io.Closeable;
+import java.lang.Iterable;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;  
+import org.apache.poi.ss.usermodel.Sheet;  
 
 import dal.DataAccessLayer;
 import view.Menu;
@@ -81,7 +90,57 @@ public class Controller {
 				}
 			}
 		});
-
-	
+		
 	}
-}
+		
+		public File chooseFile() {
+			JFileChooser fileChooser = new JFileChooser(); 
+			int returnValue = menu.getFileChooser().showOpenDialog(null);
+			if(returnValue == JFileChooser.APPROVE_OPTION) {
+				
+				File selectedFile = menu.getFileChooser().getSelectedFile(); 
+				setFile(selectedFile);
+				return selectedFile;
+			}
+			
+			return null; 
+			
+		}
+		
+		public void setFile(File selectedFile) {
+			File file = new File(selectedFile.getAbsolutePath());
+			
+			try {
+				Workbook workbook = Workbook.getWorkbook(file);
+				Sheet sheet = workbook.getSheet(0); 
+				menu.getHeaders().clear(); 
+				
+				for(int i = 0; i< sheet.getColumn(); i++) {
+					Cell cell1 = sheet.getCell(i, 0); 
+					menu.getHeaders().add(cell1, getContents());
+				}
+				
+				menu.getData().clear(); 
+			
+				for(int j = 1; j< sheet.getRows(); j++) {
+					Vector d = new Vector(); 
+					for(int i = 0; i < sheet.getColumns(); i++) {
+						Cell cell = sheet.getCell(i, j); 
+						d.add(cell.getContents()); 
+					
+				}
+				
+				d.add("\n"); 
+				menu.getData().add(d); 		
+				
+			}
+			
+				menu.setTable(); 		
+			}
+			
+			catch (Exception e) {
+				menu.getTextField().setText("Wrong format of imported file. The format must be: .xls");
+			}
+				
+		} 
+	}
--- ProgrammingAssignment3/src/view/Menu.java
+++ ProgrammingAssignment3/src/view/Menu.java
@@ -9,8 +9,13 @@ import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableModel;
+
 import java.awt.Font;
 import java.awt.Color;
 import controller.Controller;
@@ -23,7 +28,10 @@ public class Menu extends JFrame {
 	private JButton btnExcel; 
 	private JButton btnAccess; 
 	private Controller controller; 
-
+	private TableModel tableModel_content;
+	private TableModel tableModel_contentUppgift2; 
+	private JFileChooser fileChooser; 
+	private JTableHeader headers; 
 
 	public Menu() {
 		initialize();
@@ -67,6 +75,9 @@ public class Menu extends JFrame {
 		btnAccess.setFont(new Font("Verdana", Font.BOLD, 11));
 		btnAccess.setBounds(50, 110, 180, 44);
 		frmCronusSverigeab.getContentPane().add(btnAccess);
+		
+		tableModel_content = (DefaultTableModel) table_content.getModel(); 
+		tableModel_contentUppgift2 = (DefaultTableModel) table_contentUppgift2.getModel(); 
 	}
 	
 	public JFrame getFrmCronusSverigeab() {
@@ -112,5 +123,29 @@ public class Menu extends JFrame {
 	}
 	public void setBtnExcel(JButton btnExcel) {
 		this.btnExcel = btnExcel;
+	}
+	public TableModel getTableModel_content() {
+		return tableModel_content;
+	}
+	public void setTableModel_content(TableModel tableModel_content) {
+		this.tableModel_content = tableModel_content;
+	}
+	public TableModel getTableModel_contentUppgift2() {
+		return tableModel_contentUppgift2;
+	}
+	public void setTableModel_contentUppgift2(TableModel tableModel_contentUppgift2) {
+		this.tableModel_contentUppgift2 = tableModel_contentUppgift2;
+	}
+	public JFileChooser getFileChooser() {
+		return fileChooser;
+	}
+	public void setFileChooser(JFileChooser fileChooser) {
+		this.fileChooser = fileChooser;
+	}
+	public JTableHeader getHeaders() {
+		return headers;
+	}
+	public void setHeaders(JTableHeader headers) {
+		this.headers = headers;
 	}
 }
